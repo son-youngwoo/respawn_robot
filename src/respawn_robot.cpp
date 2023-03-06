250a7b4ea7886a1ef9b8b186bae9ea49ed41f031
son-youngwoo <duddn9603@g.skku.edu>
2023-03-06 16:49:40 +0900

    add yaw_target

--- src/respawn_robot.cpp
+++ src/respawn_robot.cpp
@@ -6,6 +6,9 @@
 #include <std_msgs/Float32MultiArray.h>
 #include <std_msgs/String.h>
 
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2/LinearMath/Matrix3x3.h>
+
 #include <nav_msgs/Path.h>
 #include <std_msgs/Bool.h>
 #include <std_msgs/Int8.h>
@@ -19,17 +22,27 @@ int respawn_flag = 1;
 bool get_s_or_f = 0;
 double x = 0;
 double y = 0;
+double z = 0;
 double x_thres = 0.05;
 double y_thres = 0.05;
 double roll_limit = 0.7;
 double pitch_limit = 0.7;
 double roll = 0;
 double pitch = 0;
+double yaw = 0;
 int data_id = 0;
 bool s_or_f;
 int cnt = 0;
 double timer1 = 0;
 double timer2 = 0;
+double timer3 = 0;
+double yaw_target = 0;
+double yaw_target_dis = 0;
+int k = 0;
+double rand_x_tar = 0;
+double rand_y_tar = 0;
+double x_init = 0;
+double y_init = 0;
 
 respawn_robot::dataset dataset;
 grid_map_msgs::GridMap elevation_map_raw;
@@ -41,9 +54,10 @@ void msgCallbackBodyPose(const std_msgs::Float32MultiArray::ConstPtr& msg)
 {
     x = msg->data[0];
     y = msg->data[1];
-    
+    z = msg->data[2];
     roll = msg->data[3];
     pitch = msg->data[4];
+    yaw = msg->data[5];
 
     // if (abs(roll) > roll_limit || abs(pitch) > pitch_limit || 시간이 너무많이 걸린다) {
     //     s_or_f = 0; 
@@ -92,22 +106,20 @@ int main(int argc, char** argv)
     while(ros::ok()) {
 
         if (respawn_flag == 1) { // 1. mpc 켜는 명령 보내기, id 데이터셋에 저장하기.
-            std::cout << "respawn_flag: " << respawn_flag << std::endl;        
             timer1 += 0.5;
-            std::cout << "timer1: " << timer1 << std::endl;       
                 
             if(timer1 == 2) {
-                std::cout << "timer1: " << timer1 << std::endl;
+                std::cout << "initialize imu" << std::endl;
+                controlinput.data = 2;
+                pub_controlinput.publish(controlinput);
+            }
+            else if(timer1 == 3) {
+                std::cout << "command stand mode"  << std::endl;
                 controlinput.data = 4;
                 pub_controlinput.publish(controlinput);
             }
             else if(timer1 == 4) {
-                std::cout << "timer1: " << timer1 << std::endl;
-                controlinput.data = 2;
-                pub_controlinput.publish(controlinput);
-            }
-            else if(timer1 == 6) {
-                std::cout << "timer1: " << timer1 << std::endl;
+                std::cout << "cammand mpc mode" << std::endl;
                 controlinput.data = 5;
                 pub_controlinput.publish(controlinput);
 
@@ -117,80 +129,103 @@ int main(int argc, char** argv)
 
                 respawn_flag = 2;    
             }
-            
-            // if(timer == 1) {
-            //     std::cout << "timer: " << timer << std::endl;        
-            //     controlinput.data = 1;
-            //     pub_controlinput.publish(controlinput);
-            // }
-            // else if(timer == 7) {
-            //     std::cout << "timer: " << timer << std::endl;
-            //     controlinput.data = 2;
-            //     pub_controlinput.publish(controlinput);
-            // }
-            // else if(timer == 8) {
-            //     std::cout << "timer: " << timer << std::endl;
-            //     controlinput.data = 3;
-            //     pub_controlinput.publish(controlinput);
-            // }
-            // else if(timer == 9) {
-            //     std::cout << "timer: " << timer << std::endl;
-            //     controlinput.data = 4;
-            //     pub_controlinput.publish(controlinput);
-            // }
-            // else if(timer == 10) {
-            //     std::cout << "timer: " << timer << std::endl;
-            //     controlinput.data = 5;
-            //     pub_controlinput.publish(controlinput);
-
-            //     timer = 0;
-
-            //     dataset.id = data_id;
-
-            //     respawn_flag = 2;    
-            // }
         }
         else if (respawn_flag == 2) { // 2. elevation map 데이터셋에 저장하기.
-            std::cout << "respawn_flag: " << respawn_flag << std::endl;        
+            std::cout << "get elevation map" << std::endl;        
             dataset.elevation_map_raw = elevation_map_raw;
             respawn_flag = 3;
         }
-        else if (respawn_flag == 3) {  // 3. 타겟 포지션 보내고 데이터셋에 저장하기.           
-            std::cout << "respawn_flag: " << respawn_flag << std::endl;        
+        else if (respawn_flag == 3) {  // 3. 타겟 포지션 보내고 데이터셋에 저장하기.    
+            timer2 += 0.5;
+            if(timer2 == 0.5) {       
+                std::cout << "publish target position" << std::endl;        
 
-            double radius = 5.0;
-            double centerX = 0.0;
-            double centerY = 0.0;
+                double radius = 5.0;
+                double centerX = 0.0;
+                double centerY = 0.0;
 
-            // Seed the random number generator
-            std::random_device rd_tar;
-            std::mt19937 gen_tar(rd_tar());
-            std::uniform_real_distribution<> dis_tar(0.0, 1.0);
+                // Seed the random number generator
+                std::random_device rd_tar;
+                std::mt19937 gen_tar(rd_tar());
+                std::uniform_real_distribution<> dis_tar(0.0, 1.0);
 
-            // Generate a random radius and angle
-            double r = radius * std::sqrt(dis_tar(gen_tar));
-            double theta = 2.0 * M_PI * dis_tar(gen_tar);
+                // Generate a random radius and angle
+                double r = radius * std::sqrt(dis_tar(gen_tar));
+                double theta = 2.0 * M_PI * dis_tar(gen_tar);
+                
+                x_init = x;
+                y_init = y;
 
-            // Convert polar coordinates to Cartesian coordinates
-            double rand_x_tar = centerX + r * std::cos(theta);
-            double rand_y_tar = centerY + r * std::sin(theta);
-            
-            // target x,y dataset 저장.
-            dataset.x = rand_x_tar;
-            dataset.y = rand_y_tar;
+                // Convert polar coordinates to Cartesian coordinates
+                rand_x_tar = x + r * std::cos(theta);
+                rand_y_tar = y + r * std::sin(theta);
+                
+                // target x,y dataset 저장.
+                dataset.x = rand_x_tar;
+                dataset.y = rand_y_tar;
 
-            // rand_x_tar, rand_y_tar 이용해서 path 생성.
+                yaw_target = atan2(rand_y_tar - y, rand_x_tar - x); // theta based world frame  
+                
+                yaw_target_dis = yaw_target / 10;
 
 
-            // nav_msgs::Path path;
-            // pub_path.publish(path);
+            }
+            k++;
 
-            respawn_flag = 4;
+            if(k < 11) {
+                yaw_target = yaw_target_dis*k;
+            }
+            else {
+                yaw_target = yaw_target_dis*10;
+            }
+        
+
+            tf2::Quaternion q;
+
+            q.setRPY(0, 0, yaw_target);
+
+            nav_msgs::Path path;
+
+            path.header.stamp = ros::Time::now();
+            path.header.frame_id = "world";
+            path.poses.resize(2);  // allocate memory for the poses array
+            path.poses[0].header.frame_id = "world";
+            path.poses[0].header.stamp = ros::Time::now();
+            path.poses[0].pose.position.x = x_init;
+            path.poses[0].pose.position.y = y_init;
+            path.poses[0].pose.orientation.x = 0;
+            path.poses[0].pose.orientation.y = 0;
+            path.poses[0].pose.orientation.z = 0;
+            path.poses[0].pose.orientation.w = 1;
+
+            path.poses[1].header.frame_id = "world";
+            path.poses[1].header.stamp = ros::Time::now();
+            path.poses[1].pose.position.x = rand_x_tar;
+            path.poses[1].pose.position.y = rand_y_tar;
+            path.poses[1].pose.orientation.x = q.x();
+            path.poses[1].pose.orientation.y = q.y();
+            path.poses[1].pose.orientation.z = q.z();
+            path.poses[1].pose.orientation.w = q.w();
+
+            pub_path.publish(path);
+
+            if (abs(yaw_target - yaw) < 0.1){  
+                k = 0;
+                timer2 = 0;
+
+                respawn_flag = 4;
+            }
         }
         else if(respawn_flag == 4) { // 4. 성공인지 실패인지 결과가 나오면 데이터셋에 저장하고 퍼블리시하기.
-            std::cout << "respawn_flag: " << respawn_flag << std::endl;        
+            std::cout << "move ..." << std::endl;        
 
             if (get_s_or_f == 1) {
+                if (s_or_f == 1) {
+                    std::cout << "success !" << std::endl; 
+                }
+                else {
+                    std::cout << "fail !" << std::endl; 
+                }
                 dataset.s_or_f = s_or_f;
 
                 pub_dataset.publish(dataset);
@@ -199,16 +234,42 @@ int main(int argc, char** argv)
             }
         }
         else if(respawn_flag == 5) { // 5. 로봇 crawl 자세로 바꾸기
-            std::cout << "respawn_flag: " << respawn_flag << std::endl;        
 
             controlinput.data = 3;
             pub_controlinput.publish(controlinput);
 
-            timer2 += 0.5;
-        
-            if(timer2 == 2) {
+            // nav_msgs::Path path;
+
+            // path.header.stamp = ros::Time::now();
+            // path.header.frame_id = "world";
+            // path.poses.resize(2);  // allocate memory for the poses array
+            // path.poses[0].header.frame_id = "world";
+            // path.poses[0].header.stamp = ros::Time::now();
+            // path.poses[0].pose.position.x = x_init;
+            // path.poses[0].pose.position.y = y_init;
+            // path.poses[0].pose.orientation.x = 0;
+            // path.poses[0].pose.orientation.y = 0;
+            // path.poses[0].pose.orientation.z = 0;
+            // path.poses[0].pose.orientation.w = 1;
+
+            // path.poses[1].header.frame_id = "world";
+            // path.poses[1].header.stamp = ros::Time::now();
+            // path.poses[1].pose.position.x = x;
+            // path.poses[1].pose.position.y = y;
+            // path.poses[1].pose.orientation.x = 0;
+            // path.poses[1].pose.orientation.y = 0;
+            // path.poses[1].pose.orientation.z = 0;
+            // path.poses[1].pose.orientation.w = 1;
+
+            // pub_path.publish(path);
+
+            timer3 += 0.5;
+            if(timer3 == 0.5) {
+                std::cout << "command crawl mode"  << std::endl;        
+            }
+            if(timer3 == 2) {
                 respawn_flag = 6;
-                timer2 = 0;            
+                timer3 = 0;            
             }
         }    
 
@@ -227,7 +288,7 @@ int main(int argc, char** argv)
         //     }
         // }
         else if(respawn_flag == 6) { // 6. 랜덤 포지션으로 리스폰하기.
-            std::cout << "respawn_flag: " << respawn_flag << std::endl;        
+            std::cout << "respawn ..." << std::endl;        
 
             // create a random number generator engine
             std::random_device rd_init;
@@ -254,7 +315,7 @@ int main(int argc, char** argv)
             srv.request.model_state = modelState;
             if (client.call(srv))
             {
-                ROS_INFO("Robot respawned successfully");
+                // ROS_INFO("Robot respawned successfully");
                 std::cout << "respawn x: " << rand_x_init << " y: " << rand_y_init << std::endl;        
             }
             else
